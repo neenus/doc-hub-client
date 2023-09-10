c358f9d1fbbd37e37f7d332fa70e667f110e86e4
Neenus Gabriel <hello@neenus.com>
2023-09-10 03:27:55 +0000

    refactor(app.js, index.js, layout.jsx): refactor client routes code
    refactor to work with createBrowserRouter, createRoutesFromElements and to implement protected
    routes

--- src/App.js
+++ src/App.js
@@ -2,26 +2,39 @@ import React from 'react';
 import './App.css';
 import {
   createBrowserRouter,
+  createRoutesFromElements,
   RouterProvider,
+  Route, redirect
 } from "react-router-dom";
 import Home from "./Routes/Home";
 import About from "./Routes/About";
 import ErrorPage from "./Routes/Error/Error";
+import Login from './Routes/SignIn';
 import Footer from './components/Footer/Footer.component';
+import Layout from './components/Layout';
 import { Container } from '@mui/material';
 import { Box } from '@mui/system';
+import { isAuth } from './utils/isAuth';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  }
-]);
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path="/" element={<Layout />}>
+    <Route index element={<Home />} loader={async () => {
+      try {
+        if (!await isAuth()) {
+          return redirect('/login');
+        }
+        return null;
+      } catch (error) {
+        console.log({ error })
+        throw redirect('/login');
+      }
+    }}
+    />
+    <Route path="about" element={<About />} />
+    <Route path="login" element={<Login />} />
+    <Route path="*" element={<ErrorPage />} />
+  </Route>
+));
 
 function App() {
   return (
