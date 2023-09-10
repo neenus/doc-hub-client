629ad5898f8953d5a2d7a81405ef7d258e5b67fe
Neenus Gabriel <hello@neenus.com>
2023-09-10 16:03:38 +0000

    refactor(errorpage, about): rename about and error pages to tsx files

--- src/Routes/About.tsx
+++ src/Routes/About.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
     <React.Fragment>
       <h1>This is the about page</h1>
     </React.Fragment>
-  )
-}
+  );
+};
 
-export default About
+export default About;
