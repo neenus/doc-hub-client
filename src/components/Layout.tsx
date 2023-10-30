ccadd0d0a4f26bcf9f127f645584cf497e676f34
Neenus Gabriel <hello@neenus.com>
2023-10-30 13:59:47 +0000

    feat(header): add header section to the app

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Header from "./Header/Header.component";
 
 const Layout = () => {
   return (
     <div>
+      <Header />
       <Outlet />
     </div>
   );
-}
+};
 
 export default Layout;
