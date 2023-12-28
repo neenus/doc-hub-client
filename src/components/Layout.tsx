0b246b716921d6734c71593efbfd1a774e30ce3e
Neenus Gabriel <hello@neenus.com>
2023-12-28 00:42:01 +0000

    refactor(layout): add loader suspense and loader fallback to layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
+import { Suspense } from "react";
 import Header from "./Header/Header.component";
+import Loader from "./Loader/Loader.component";
 
 const Layout = () => {
   return (
     <div>
       <Header />
-      <Outlet />
+      <Suspense fallback={<Loader loading={true} />}>
+        <Outlet />
+      </Suspense>
     </div>
   );
 };
