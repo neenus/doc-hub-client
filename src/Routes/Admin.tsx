1b1f6cdd625c69b900c12ecd5496961d5cbfb8f9
Neenus Gabriel <hello@neenus.com>
2024-05-09 20:15:21 +0000

    refactor(admin): refactor admin panel to use mui data-grid

--- src/Routes/Admin.tsx
+++ src/Routes/Admin.tsx
@@ -1,13 +1,13 @@
 import { useEffect } from "react";
 import { selectAuth } from "../features/auth/authSlice";
-import { TableContainer, Table, TableHead, TableBody, TableCell, TableRow, Paper, IconButton, Fab } from "@mui/material";
+import { Box, Typography, IconButton, Fab } from "@mui/material";
+import { DataGrid } from "@mui/x-data-grid";
 import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import AddIcon from '@mui/icons-material/Add';
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getUsers } from "../features/users/userSlice";
-import { User } from "../types";
 
 const Admin = () => {
 
@@ -16,74 +16,80 @@ const Admin = () => {
   const users = useSelector((state: any) => state.users.users);
   const navigate = useNavigate();
 
+  const columns = [
+    { field: "name", headerName: "Name", flex: 1 },
+    { field: "email", headerName: "Email", flex: 1 },
+    { field: "role", headerName: "Role", flex: 1 },
+    { field: "dir", headerName: "User Directory", flex: 1 },
+    {
+      field: "actions", headerName: "Actions", renderCell: (params: any) => (
+        <>
+          <IconButton color="primary" onClick={() => navigate(`/admin/edit-user/${params.row._id}`, {
+            state: params.row
+          })}>
+            <EditIcon />
+          </IconButton>
+          <IconButton color="warning" onClick={() => console.log("Delete User: ", params.row)}>
+            <DeleteIcon />
+          </IconButton>
+        </>
+      )
+    }
+  ];
+
   const handleAddUser = () => navigate("/admin/add-user");
 
-  const handleNavigation = (path: string, user: User) => {
-    navigate(path, { state: user });
-  }
-
-
   useEffect(() => {
-    if (auth.user?.role !== "admin") {
-      return navigate("/", { replace: true });
-    }
+    if (auth.user?.role !== "admin") navigate("/", { replace: true });
     dispatch(getUsers());
   }, [auth]);
 
   return (
     <>
-      <TableContainer component={Paper} sx={{ marginTop: "1rem", marginBottom: "1rem" }}>
-        <Table>
-          <TableHead sx={{
-            "& .MuiTableCell-root": {
-              fontWeight: "bold",
-              textAlign: "center"
-            }
-          }}
-            color="primary"
+      <Box>
+        <Box py={5} sx={{ textAlign: "center" }}>
+          <Typography
+            variant="h5"
+            gutterBottom
+            sx={{
+              color: "#0B096A",
+              textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+              lineHeigh: "22px",
+              fontWeight: 700
+            }}
           >
-            <TableRow>
-              {/* <TableCell>
-                <Checkbox />
-              </TableCell> */}
-              <TableCell>Name</TableCell>
-              <TableCell>Email</TableCell>
-              <TableCell>Role</TableCell>
-              <TableCell>Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody sx={{
-            "& .MuiTableCell-root": {
-              textAlign: "center"
-            }
-
-          }}>
-            {users.map((user: any) => (
-              <TableRow
-                key={user._id}
-                onClick={() => handleNavigation(`/myfiles`, user)}
-              >
-                {/* <TableCell>
-                  <Checkbox />
-                </TableCell> */}
-                <TableCell>{user.name}</TableCell>
-                <TableCell>{user.email}</TableCell>
-                <TableCell>{user.role}</TableCell>
-                <TableCell>
-                  <IconButton sx={{
-                    cursor: "not-allowed"
-                  }}>
-                    <EditIcon color="primary" />
-                  </IconButton>
-                  <IconButton>
-                    <DeleteIcon color="error" />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            Doc-Hub Admin Panel
+          </Typography>
+        </Box>
+        <Box sx={{
+          "& .MuiDataGrid-root": {
+            height: "60vh",
+            width: "100%",
+            backgroundColor: "#fff",
+          },
+          "& .MuiDataGrid-columnHeader": {
+            backgroundColor: "#f4f4f4",
+          },
+          "& .MuiDataGrid-footerContainer": {
+            backgroundColor: "#f4f4f4",
+          },
+          "& .MuiDataGrid-virtualScroller": {
+            backgroundColor: "#fefefe",
+          },
+        }}>
+          <DataGrid
+            initialState={{
+              pagination: {
+                paginationModel: { pageSize: 10, page: 1 }
+              },
+            }}
+            rows={users}
+            columns={columns}
+            getRowId={(row) => row._id}
+            pageSizeOptions={[5, 10, 25, 50]}
+          />
+        </Box>
+      </Box>
       <Fab color="primary" aria-label="add" sx={{ position: "fixed", bottom: "2rem", right: "2rem" }} onClick={handleAddUser}>
         <AddIcon />
       </Fab>
